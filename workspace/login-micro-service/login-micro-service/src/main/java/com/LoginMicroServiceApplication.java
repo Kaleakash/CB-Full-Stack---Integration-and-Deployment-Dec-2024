1bd9bf4e33df2b83c4733421f53f9aa9df7cd210
Akash <akash300383@gmail.com>
2025-07-04 09:21:36 +0530

    done

--- workspace/login-micro-service/login-micro-service/src/main/java/com/LoginMicroServiceApplication.java
+++ workspace/login-micro-service/login-micro-service/src/main/java/com/LoginMicroServiceApplication.java
@@ -1,10 +1,18 @@
 package com;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import com.entity.Login;
+import com.repository.LoginRepository;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication(scanBasePackages = "com") // Enable @RestController, @Service, @Repository 
 @EntityScan(basePackages = "com.entity")			// Enable @Entity annotation 
@@ -15,6 +23,28 @@ public class LoginMicroServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LoginMicroServiceApplication.class, args);
 		System.out.println("Login micro service up on port number 9090");
+	}
+	
+	
+	@Autowired
+	LoginRepository loginRepository;
+	
+	@PostConstruct
+	public void init() {
+		System.out.println("this method called..");
+		Login ll = new Login();
+		ll.setEmailid("admin@gmail.com");
+		ll.setPassword("admin@123");
+		ll.setTypeofuser("admin");
+		
+		Optional<Login> result = loginRepository.findById(ll.getEmailid());
+		if(result.isPresent()) {
+			System.out.println("Admin account present");
+		}else {
+			loginRepository.save(ll);
+			System.out.println("Admin account created..");
+		}
+		
 	}
 
 }
